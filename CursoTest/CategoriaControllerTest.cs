12beb31a68ba154be82a9a02ed1a25c40899e89d
dwenndson <diego.wenndson@gmail.com>
2021-11-05 16:33:03 -0300

    Adicionar arquivos de projeto.

---
+++ CursoTest/CategoriaControllerTest.cs
@@ -0,0 +1,26 @@
+﻿using CursoMVC.Models;
+using CursoMVC.Models.Contexto;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoTest
+{
+    public class CategoriaControllerTest
+    {
+        private readonly Mock<DbSet<Categoria>> _mockSet;
+        private readonly Mock<Contexto> _mockContexto;
+        private readonly Categoria _categoria;
+        
+        public CategoriaControllerTest ()
+        {
+            _mockSet = new Mock<DbSet<Categoria>>();
+            _mockContexto = new Mock<Contexto>();
+            _categoria = new Categoria { Id = 1, Descricao = "Teste Categoria" };
+        }
+    }
+}
